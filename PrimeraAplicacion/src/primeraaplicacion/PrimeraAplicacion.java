8e743c92eea7042de6182b303a3cbe04ea0ab0e2
ivanledokami@gmail.com <MSI@DESKTOP-N71TPMD>
2018-09-26 13:22:09 +0200

    Hola mundo

--- PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
+++ PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
@@ -15,7 +15,7 @@ public class PrimeraAplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo") ;
     }
     
 }
